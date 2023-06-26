c2b71647e5081020e4833dbf53b2d3a8f5f6c42e
DorinBe <dorinbeery@gmail.com>
2023-06-26 21:57:28 +0300

    add validation for sign up

--- src/views/javascript/register.js
+++ src/views/javascript/register.js
@@ -31,8 +31,33 @@ $('#register').click((e) => {
   const password = $('#pwd').val();
   const repeatPassword = $('#rpwd').val();
 
+  let hasUpperCase = /[A-Z]/.test(password);
+  let hasLowerCase = /[a-z]/.test(password);
+  let hasNumbers = /\d/.test(password);
+  let hasNonalphas = /\W/.test(password);
+  let isLengthValid = password.length >= 6;
+
+  if (!(hasUpperCase && hasLowerCase && hasNumbers && hasNonalphas && isLengthValid)) {
+    alert('Password is not valid, please make sure of the following:\n' +
+      '1. Password should contain at least one uppercase letter.\n' +
+      '2. Password should contain at least one lowercase letter.\n' +
+      '3. Password should contain at least one number.\n' +
+      '4. Password should contain at least one special character.\n' +
+      '5. Password should be at least 6 characters long.');
+    return;
+  }
+
+
   if (password !== repeatPassword) {
     alert('Passwords do not match');
+    return;
+  }
+
+  let isEmailValid = /^\S+@\S+\.\S+$/.test(email);
+  if (!isEmailValid) {
+    alert('Email is not valid, please make sure of the following:\n' +
+      '1. Email should contain @ and .\n' +
+      '2. Email should not contain spaces.');
     return;
   }
 
